a4933d7c0a14fe8cb081294104f4145f806659e6
夏先生 <lessissmore@163.com>
2021-07-02 15:45:54 +0800

    add typeOf.js

--- js/ECMAScript/prototype.js
+++ js/ECMAScript/prototype.js
@@ -10,3 +10,29 @@ function Son(age){
 Son.prototype = new Father('father')
 let s = new Son(11);
 s.sayName();
+
+function Animal(name) {
+    this.name = name
+    this.colors = ['black', 'white']
+}
+function Animal(name) {
+    this.name = name
+    this.getName = function() {
+        return this.name
+    }
+}
+function Dog(name) {
+    Animal.call(this, name)
+}
+// 寄生式组合继承
+function object(o) {
+    function F() {}
+    F.prototype = o
+    return new F()
+}
+function inheritPrototype(child, parent) {
+    let prototype = object(parent.prototype)
+    prototype.constructor = child
+    child.prototype = prototype
+}
+inheritPrototype(Dog, Animal)
