0645c1f445161c3a7368749db7c4a9b0f8daa73a
夏先生 <lessissmore@163.com>
2021-09-02 15:27:55 +0800

    new

--- js/utils/throttle.js
+++ js/utils/throttle.js
@@ -32,4 +32,18 @@ function debounce(fn, time) {
         }, time)
     }
 }
+function New(func) {
+    var res = {};
+    if (func.prototype !== null) {
+        res.__proto__ = func.prototype;
+    }
+    var ret = func.apply(res, Array.prototype.slice.call(arguments, 1));
+    if ((typeof ret === "object" || typeof ret === "function") && ret !== null) {
+        return ret;
+    }
+    return res;
+}
+var obj = New(A, 1, 2);
+// equals to
+var obj = new A(1, 2);
 
