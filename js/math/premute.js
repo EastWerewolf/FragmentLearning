29e3ac6b1f8e5337f414c8c541398e11fd21fe0e
夏先生 <lessissmore@163.com>
2021-09-03 15:11:08 +0800

    getKthFromEnd

--- js/math/premute.js
+++ js/math/premute.js
@@ -55,3 +55,23 @@ function permute1(nums){
     return res
 }
 permute1([1,2,3,4])
+
+// 给定一个链表: 1->2->3->4->5, 和 k = 2.
+//
+// 返回链表 4->5.
+const getKthFromEnd = (head, k) => {
+    let fast, slow;
+    fast = slow = head;
+    while (k--) {
+        // 快指针先走k步
+        fast = fast.next;
+    }
+    while (fast) {
+        // 再一起走，直到快指针走到头
+        fast = fast.next;
+        slow = slow.next;
+    }
+    // 此时的慢指针指的就是倒数第k个
+    return slow;
+};
+
