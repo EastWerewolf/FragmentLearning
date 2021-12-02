ace10815b8d22795be0c72f2dba3dd08b710d871
夏先生 <lessissmore@163.com>
2021-12-02 13:54:25 +0800

    Check if a year is leap year

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -218,3 +218,13 @@ const contains = (arr, criteria) => arr.filter(criteria).length > 0;
 contains([10, 20, 30], (v) => v > 25); // true
 contains([10, 20, 30], (v) => v > 100 || v < 15); // true
 contains([10, 20, 30], (v) => v > 100); // false
+
+/**
+ * Check if a year is leap year
+ * @param year
+ * @returns {boolean}
+ */
+const isLeapYear = (year) => (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+// Or
+// Get the number of days in February
+const isLeapYear = (year) => new Date(year, 1, 29).getDate() === 29;
