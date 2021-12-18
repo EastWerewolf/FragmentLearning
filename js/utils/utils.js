3bd88aef8d232008e6e2c0ea4659f8fd209d337e
夏先生 <lessissmore@163.com>
2021-12-18 13:12:06 +0800

    Renders the given DOM tree in the specified DOM element.

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -411,3 +411,42 @@ const parseCookie = str =>
             return acc;
         }, {});
 parseCookie('foo=bar; equation=E%3Dmc%5E2');
+
+/**
+ * Renders the given DOM tree in the specified DOM element.
+ * @param {*} param0 
+ * @param {*} container 
+ */
+const renderElement = ({ type, props = {} }, container) => {
+  const isTextElement = !type;
+  const element = isTextElement
+    ? document.createTextNode('')
+    : document.createElement(type);
+
+  const isListener = p => p.startsWith('on');
+  const isAttribute = p => !isListener(p) && p !== 'children';
+
+  Object.keys(props).forEach(p => {
+    if (isAttribute(p)) element[p] = props[p];
+    if (!isTextElement && isListener(p))
+      element.addEventListener(p.toLowerCase().slice(2), props[p]);
+  });
+
+  if (!isTextElement && props.children && props.children.length)
+    props.children.forEach(childElement =>
+      renderElement(childElement, element)
+    );
+
+  container.appendChild(element);
+};
+const myElement = {
+  type: 'button',
+  props: {
+    type: 'button',
+    className: 'btn',
+    onClick: () => alert('Clicked'),
+    children: [{ props: { nodeValue: 'Click me' } }]
+  }
+};
+
+renderElement(myElement, document.body);
