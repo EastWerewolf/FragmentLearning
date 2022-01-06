84e331d63632d034794a9ef79f6887138b0fdc47
夏先生 <lessissmore@163.com>
2022-01-06 09:49:50 +0800

    JavaScript Data Structures - Binary Tree

---
+++ js/dataStructure/treeData.js
@@ -0,0 +1,103 @@
+/*
+二叉树数据结构中的每个节点都必须具有以下属性：
+
+key: 节点的key
+value: 节点的值
+parent: 节点的父节点（null如果没有）
+left: 指向节点左子节点的指针（null如果没有）
+right: 指向节点右孩子的指针（null如果没有）
+二叉树数据结构的主要操作有：
+
+insert: 插入一个节点作为给定父节点的子节点
+remove: 从二叉树中删除一个节点及其子节点
+find: 检索给定节点
+preOrderTraversal: 通过递归遍历每个节点及其子节点来遍历二叉树
+postOrderTraversal: 通过递归遍历每个节点的子节点后跟节点来遍历二叉树
+inOrderTraversal: 通过递归遍历每个节点的左孩子，然后是节点，然后是右孩子来遍历二叉树
+ */
+class BinaryTreeNode {
+    constructor(key, value = key, parent = null) {
+        this.key = key;
+        this.value = value;
+        this.parent = parent;
+        this.left = null;
+        this.right = null;
+    }
+
+    get isLeaf() {
+        return this.left === null && this.right === null;
+    }
+
+    get hasChildren() {
+        return !this.isLeaf;
+    }
+}
+
+class BinaryTree {
+    constructor(key, value = key) {
+        this.root = new BinaryTreeNode(key, value);
+    }
+
+    *inOrderTraversal(node = this.root) {
+        if (node.left) yield* this.inOrderTraversal(node.left);
+        yield node;
+        if (node.right) yield* this.inOrderTraversal(node.right);
+    }
+
+    *postOrderTraversal(node = this.root) {
+        if (node.left) yield* this.postOrderTraversal(node.left);
+        if (node.right) yield* this.postOrderTraversal(node.right);
+        yield node;
+    }
+
+    *preOrderTraversal(node = this.root) {
+        yield node;
+        if (node.left) yield* this.preOrderTraversal(node.left);
+        if (node.right) yield* this.preOrderTraversal(node.right);
+    }
+
+    insert(
+        parentNodeKey,
+        key,
+        value = key,
+        { left, right } = { left: true, right: true }
+    ) {
+        for (let node of this.preOrderTraversal()) {
+            if (node.key === parentNodeKey) {
+                const canInsertLeft = left && node.left === null;
+                const canInsertRight = right && node.right === null;
+                if (!canInsertLeft && !canInsertRight) return false;
+                if (canInsertLeft) {
+                    node.left = new BinaryTreeNode(key, value, node);
+                    return true;
+                }
+                if (canInsertRight) {
+                    node.right = new BinaryTreeNode(key, value, node);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    remove(key) {
+        for (let node of this.preOrderTraversal()) {
+            if (node.left.key === key) {
+                node.left = null;
+                return true;
+            }
+            if (node.right.key === key) {
+                node.right = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    find(key) {
+        for (let node of this.preOrderTraversal()) {
+            if (node.key === key) return node;
+        }
+        return undefined;
+    }
+}
