052050f1d25834ad3dbaa40b3886bb6fddd834d4
夏先生 <lessissmore@163.com>
2022-01-11 11:27:09 +0800

    Deep maps an object's keys

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -899,3 +899,49 @@ const queryStringToObject = url =>
         {}
     );
 queryStringToObject('https://google.com?page=1&count=10');
+
+/**
+ * Deep maps an object's keys
+ * @param obj
+ * @param fn
+ * @returns {any}
+ */
+const deepMapKeys = (obj, fn) =>
+    Array.isArray(obj)
+        ? obj.map(val => deepMapKeys(val, fn))
+        : typeof obj === 'object'
+        ? Object.keys(obj).reduce((acc, current) => {
+            const key = fn(current);
+            const val = obj[current];
+            acc[key] =
+                val !== null && typeof val === 'object' ? deepMapKeys(val, fn) : val;
+            return acc;
+        }, {})
+        : obj;
+const obj = {
+    foo: '1',
+    nested: {
+        child: {
+            withArray: [
+                {
+                    grandChild: ['hello']
+                }
+            ]
+        }
+    }
+};
+const upperKeysObj = deepMapKeys(obj, key => key.toUpperCase());
+/*
+{
+  "FOO":"1",
+  "NESTED":{
+    "CHILD":{
+      "WITHARRAY":[
+        {
+          "GRANDCHILD":[ 'hello' ]
+        }
+      ]
+    }
+  }
+}
+*/
