c1ff43fb19954fef3c6c3de7625c4dbcd2d30e04
夏先生 <lessissmore@163.com>
2022-01-12 13:48:47 +0800

    Gets the target value in a nested JSON object, based on the given key

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -945,3 +945,25 @@ const upperKeysObj = deepMapKeys(obj, key => key.toUpperCase());
   }
 }
 */
+/**
+ * Gets the target value in a nested JSON object, based on the given key
+ * @param obj
+ * @param target
+ * @returns {*}
+ */
+const dig = (obj, target) =>
+    target in obj
+        ? obj[target]
+        : Object.values(obj).reduce((acc, val) => {
+            if (acc !== undefined) return acc;
+            if (typeof val === 'object') return dig(val, target);
+        }, undefined);
+const data = {
+    level1: {
+        level2: {
+            level3: 'some data'
+        }
+    }
+};
+dig(data, 'level3'); // 'some data'
+dig(data, 'level4'); // undefined
