d0d1b254bd4a5e962d884f50b7905205ac5304e9
夏先生 <lessissmore@163.com>
2022-01-21 13:43:19 +0800

    Generate a series of colors from a given color

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1040,8 +1040,8 @@ const nestedComments = nest(comments);
 
 /**
  * Checks if the passed value is an object or not
- * @param {*} obj 
- * @returns 
+ * @param {*} obj
+ * @returns
  */
 const isObject = obj => obj === Object(obj);
 isObject([1, 2, 3, 4]); // true
@@ -1050,4 +1050,29 @@ isObject(['Hello!']); // true
 isObject({ a: 1 }); // true
 isObject({}); // true
 isObject(true); // false
+
+/**
+ * 16进制颜色转rgb
+ * @param hex
+ * @returns {string}
+ */
+const hexToRGB = hex => {
+    let alpha = false,
+        h = hex.slice(hex.startsWith('#') ? 1 : 0);
+    if (h.length === 3) h = [...h].map(x => x + x).join('');
+    else if (h.length === 8) alpha = true;
+    h = parseInt(h, 16);
+    return (
+        'rgb' +
+        (alpha ? 'a' : '') +
+        '(' +
+        (h >>> (alpha ? 24 : 16)) +
+        ', ' +
+        ((h & (alpha ? 0x00ff0000 : 0x00ff00)) >>> (alpha ? 16 : 8)) +
+        ', ' +
+        ((h & (alpha ? 0x0000ff00 : 0x0000ff)) >>> (alpha ? 8 : 0)) +
+        (alpha ? `, ${h & 0x000000ff}` : '') +
+        ')'
+    );
+};
 
