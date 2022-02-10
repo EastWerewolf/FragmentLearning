c22881e9e4cb9ee96d5776fa8b71e311d12742ae
夏先生 <lessissmore@163.com>
2022-02-10 16:51:06 +0800

    不要使用内置的Readonly<T>，自己实现一个

--- ts/lesson1.ts
+++ ts/lesson1.ts
@@ -14,3 +14,21 @@ const todo: TodoPreview = {
     title: 'Clean room',
     completed: false,
 }
+
+// 不要使用内置的Readonly<T>，自己实现一个。
+// 该 Readonly 会接收一个 泛型参数，并返回一个完全一样的类型，只是所有属性都会被 readonly 所修饰。
+// 也就是不可以再对该对象的属性赋值。
+interface Todo1 {
+    title: string
+    description: string
+}
+type MyReadonly<T> = {
+    readonly [P in keyof T]:T[P]
+}
+const todo1: MyReadonly<Todo1> = {
+    title: "Hey",
+    description: "foobar"
+}
+
+todo1.title = "Hello" // Error: cannot reassign a readonly property
+todo1.description = "barFoo" // Error: cannot reassign a readonly property
