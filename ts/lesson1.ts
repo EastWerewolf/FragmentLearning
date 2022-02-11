0410a5d9af6705a96b37282e40541fd17bf1be18
夏先生 <lessissmore@163.com>
2022-02-11 15:01:59 +0800

    创建一个通用的Length，接受一个readonly的数组，返回这个数组的长度。

--- ts/lesson1.ts
+++ ts/lesson1.ts
@@ -57,3 +57,14 @@ type First<T extends any[]> = T extends never[] ? never : T[0]
 
 type head1 = First<arr1> // expected to be 'a'
 type head2 = First<arr2> // expected to be 3
+
+
+// 创建一个通用的Length，接受一个readonly的数组，返回这个数组的长度。
+
+type tesla = ['tesla', 'model 3', 'model X', 'model Y']
+type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT']
+
+type Length<T extends readonly any[]> = T['length']
+
+type teslaLength = Length<tesla> // expected 4
+type spaceXLength = Length<spaceX> // expected 5
