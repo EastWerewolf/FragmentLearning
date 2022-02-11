44f036f4f36a83601b7fa9567cc03166605d7f59
夏先生 <lessissmore@163.com>
2022-02-11 17:27:02 +0800

    优化

--- ts/lesson1.ts
+++ ts/lesson1.ts
@@ -64,6 +64,7 @@ type head2 = First<arr2> // expected to be 3
 type tesla = ['tesla', 'model 3', 'model X', 'model Y']
 type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT']
 
+// 答案
 type Length<T extends readonly any[]> = T['length']
 
 type teslaLength = Length<tesla> // expected 4
