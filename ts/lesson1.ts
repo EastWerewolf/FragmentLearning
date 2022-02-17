a55a47492e64a966b6781082e68badd6049a8503
夏先生 <lessissmore@163.com>
2022-02-17 14:51:26 +0800

    不使用 ReturnType 实现 TypeScript 的 ReturnType<T> 泛型。

--- ts/lesson1.ts
+++ ts/lesson1.ts
@@ -142,3 +142,18 @@ type Result = Unshift<[1, 2], 0> // [0, 1, 2,]
 
 // 答案
 type MyParameters<T extends (...args: any[]) => any> = T extends (...args: infer P) => any ? P : never
+
+
+// 不使用 ReturnType 实现 TypeScript 的 ReturnType<T> 泛型。
+
+// 答案
+type MyReturnType<T extends (...args: any)=> any> =  T extends (...args: any) => infer P ? P : never
+
+const fn = (v: boolean) => {
+    if (v)
+        return 1
+    else
+        return 2
+}
+
+type a = MyReturnType<typeof fn> // 应推导出 "1 | 2"
