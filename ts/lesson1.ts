729dac8c38a0dbb450ca2e02a42207b158105824
夏先生 <lessissmore@163.com>
2022-02-22 11:19:23 +0800

    实现泛型TupleToUnion<T>，它返回元组所有值的合集。

--- ts/lesson1.ts
+++ ts/lesson1.ts
@@ -209,3 +209,15 @@ const todo3: MyReadonly2<Todo, 'title' | 'description'> = {
 todo3.title = "Hello" // Error: cannot reassign a readonly property
 todo3.description = "barFoo" // Error: cannot reassign a readonly property
 todo3.completed = true // OK
+
+
+// 实现泛型TupleToUnion<T>，它返回元组所有值的合集。
+
+// 答案
+// @ts-ignore
+type TupleToUnion<T extends any[]> = T extends [infer F, ...infer R] ? F | TupleToUnion<R> : never
+
+// 例如
+type Arr = ['1', '2', '3']
+
+type Test = TupleToUnion<Arr> // expected to be '1' | '2' | '3'
