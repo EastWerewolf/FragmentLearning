3dfb4fbade9c384ed713e418af784a46f967482c
夏先生 <lessissmore@163.com>
2022-02-23 16:25:24 +0800

    实现一个通用Last<T>，它接受一个数组T并返回其最后一个元素的类型。

--- ts/lesson1.ts
+++ ts/lesson1.ts
@@ -248,5 +248,14 @@ const result1:Result1 = config
     .get()
 
 
+// 实现一个通用Last<T>，它接受一个数组T并返回其最后一个元素的类型。
 
+type Last<T extends any[]> = T extends [...infer X , infer Result] ? Result : unknown;
+// 例如
+
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+
+type tail1 = Last<arr1> // expected to be 'c'
+type tail2 = Last<arr2> // expected to be 1
 
