140c55c01ac1fe575703411bdad87283adc56f48
夏先生 <lessissmore@163.com>
2022-02-26 23:50:09 +0800

    根据其属性在并集中查找类型

--- ts/lesson1.ts
+++ ts/lesson1.ts
@@ -289,3 +289,23 @@ const promise3 = new Promise<string>((resolve, reject) => {
 // expected to be `Promise<[number, number, string]>`
 const p = Promise.all([promise1, promise2, promise3] as const)
 const p1 = PromiseAll([promise1, promise2, promise3] as const)
+
+
+// 有时，您可能希望根据其属性在并集中查找类型。
+
+// 在此挑战中，我们想通过在联合Cat | Dog中搜索公共type字段来获取相应的类型。换句话说，在以下示例中，我们期望LookUp<Dog | Cat, 'dog'>获得Dog，LookUp<Dog | Cat, 'cat'>获得Cat
+
+type LookUp<U, T> = U extends { type: T } ? U : never;
+
+interface Cat {
+    type: 'cat'
+    breeds: 'Abyssinian' | 'Shorthair' | 'Curl' | 'Bengal'
+  }
+  
+  interface Dog {
+    type: 'dog'
+    breeds: 'Hound' | 'Brittany' | 'Bulldog' | 'Boxer'
+    color: 'brown' | 'white' | 'black'
+  }
+  
+  type MyDog = LookUp<Cat | Dog, 'dog'> // expected to be `Dog`
