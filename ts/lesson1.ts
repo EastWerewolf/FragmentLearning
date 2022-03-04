6e2ee46713ec313d57c67d9c149118af82576d14
夏先生 <lessissmore@163.com>
2022-03-04 23:41:29 +0800

    实现一个范型 AppendArgument<Fn, A>，对于给定的函数类型 Fn，以及一个任意类型 A，返回一个新的函数 G。G 拥有 Fn 的所有参数并在末尾追加类型为 A 的参数。

--- ts/lesson1.ts
+++ ts/lesson1.ts
@@ -366,3 +366,12 @@ R extends ${infer L1}${From}${infer R1} ? ${L}${To}${L1}${To}${ReplaceAll<${R1},
 // 例如
 
 type replaced1= ReplaceAll<'t y p e s', ' ', ''> // 期望是 'types'
+
+
+// 实现一个范型 AppendArgument<Fn, A>，对于给定的函数类型 Fn，以及一个任意类型 A，返回一个新的函数 G。G
+// 拥有 Fn 的所有参数并在末尾追加类型为 A 的参数。
+type AppendArgument<Fn, A> = Fn extends (...args: infer R) => infer T ? (...args: [...R, A]) => T : never;
+type Fn = (a: number, b: string) => number
+
+type Result11 = AppendArgument<Fn, boolean> 
+// 期望是 (a: number, b: string, x: boolean) => number
