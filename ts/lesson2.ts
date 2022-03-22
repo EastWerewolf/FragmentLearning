ab70799fd309b83267b41e937691de1a7c9d0dd4
夏先生 <lessissmore@163.com>
2022-03-22 10:08:45 +0800

    实现PercentageParser。根据/^（\+\\-）？（\d*）？(\%)?$/ 规律性匹配T并获得三个匹配。

--- ts/lesson2.ts
+++ ts/lesson2.ts
@@ -1,6 +1,19 @@
 // 实现PercentageParser。根据/^（\+\\-）？（\d*）？(\%)?$/ 规律性匹配T并获得三个匹配。
 // 结构应该是：[加号或减号、数字、单位]如果未捕获，则默认为空字符串。
 
-
+// 答案 
+type TSplitPm<S extends string> = S extends `${infer H}${infer R}` ? (H extends '+' | '-' ? [H, R] : ['', S]) : ['', '']
+type PercentageParser<A extends string> = A extends `${infer F}%` ? [...TSplitPm<F>, '%'] : [...TSplitPm<A>, '']
 
 // 例如：
+type PString1 = ''
+type PString2 = '+85%'
+type PString3 = '-85%'
+type PString4 = '85%'
+type PString5 = '85'
+
+type R1 = PercentageParser<PString1>  // expected ['', '', '']
+type R2 = PercentageParser<PString2>  // expected ["+", "85", "%"]
+type R3 = PercentageParser<PString3>  // expected ["-", "85", "%"]
+type R4 = PercentageParser<PString4>  // expected ["", "85", "%"]
+type R5 = PercentageParser<PString5>  // expected ["", "85", ""]
