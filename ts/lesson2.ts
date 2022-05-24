fe847fda1ee38f8098f3242863f7b84ec70380ad
夏先生 <lessissmore@163.com>
2022-05-24 19:52:42 +0800

    实现一个类型 IsTuple，它接受一个输入类型 T 并返回 T 是否为元组类型

--- ts/lesson2.ts
+++ ts/lesson2.ts
@@ -354,3 +354,23 @@ type Zip<A extends any[], B extends any[], R extends any[] = []> =
   : R
 // 例如
 type exp = Zip<[1, 2], [true, false]> // expected to be [[1, true], [2, false]]
+
+
+// 实现一个类型 IsTuple，它接受一个输入类型 T 并返回 T 是否为元组类型。
+
+// 答案
+type IsTuple<T> =
+  T extends any[]
+    ? T extends []
+      ? true
+      : GreaterThan<T['length'], 0> extends true
+        ? true
+        : false
+    : T extends readonly any[]
+      ? true
+      : false
+
+// 例如：
+type case1 = IsTuple<[number]> // true
+type case2 = IsTuple<readonly [number]> // true
+type case3 = IsTuple<number[]> // false
