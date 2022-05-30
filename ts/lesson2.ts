fca9cb67b316bc1f3681b6beaed2d99344755bbe
夏先生 <lessissmore@163.com>
2022-05-30 18:41:14 +0800

    实现 TrimRight<T> ，它接收确定的字符串类型并返回一个新的字符串，其中新返回的字符串删除了原字符串结尾的空白字符串

--- ts/lesson2.ts
+++ ts/lesson2.ts
@@ -417,3 +417,17 @@ type Fill<
       
 // 例如
 type exp = Fill<[1, 2, 3], 0> // expected to be [0, 0, 0]
+
+
+// 实现 TrimRight<T> ，它接收确定的字符串类型并返回一个新的字符串，其中新返回的字符串删除了原字符串结尾的空白字符串。
+
+// 答案
+type TrimRight<S extends string> = S extends `${infer Left}${
+  | " "
+  | "\t"
+  | "\n"
+  | "\r"}`
+  ? TrimRight<Left>
+  : S;
+// 例如
+type Trimed = TrimLeft<'  Hello World  '> // 应推导出 '  Hello World'
