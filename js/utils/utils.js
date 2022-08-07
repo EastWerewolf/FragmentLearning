4b5d094d6ed8c099237fd5efe786c22c47482b98
夏先生 <lessissmore@163.com>
2022-08-07 22:23:48 +0800

    日期格式化

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1170,3 +1170,30 @@ export const handleFloatCount = (value: number | null,obj:any,key:string,count:n
       obj[key] = value;
     }
   };
+
+/**
+ * 日期格式化
+ * @param {*} value 
+ * @param {*} fmt 
+ * @returns 
+ */
+  export const dateFormat = (value: number | string | Date | null, fmt: string) => {
+    if (!value) return '';
+    const date = new Date(value);
+    const o: obj = {
+      'M+': date.getMonth() + 1, // 月份
+      'd+': date.getDate(), // 日
+      'h+': date.getHours(), // 小时
+      'm+': date.getMinutes(), // 分
+      's+': date.getSeconds(), // 秒
+      'q+': Math.floor((date.getMonth() + 3) / 3), // 季度
+      S: date.getMilliseconds() // 毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, `${date.getFullYear()}`.substr(4 - RegExp.$1.length));
+    for (const k in o) {
+      if (new RegExp(`(${k})`).test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : `00${o[k]}`.substr(`${o[k]}`.length));
+      }
+    }
+    return fmt;
+  };
