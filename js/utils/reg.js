df36f77504efa6d1cad0cc4049f1f47f074dc5a1
夏先生 <lessissmore@163.com>
2022-08-15 20:17:29 +0800

    HTML 反转义

--- js/utils/reg.js
+++ js/utils/reg.js
@@ -110,3 +110,31 @@ console.log(escape(`
 */
 
 
+// HTML 反转义
+// 有了正向的转义，就有反向的逆转义，操作如下：
+const unescape = (string) => {
+  const unescapeMaps = {
+    'amp': '&',
+    'lt': '<',
+    'gt': '>',
+    'quot': '"',
+    '#39': "'"
+  }
+  const unescapeRegexp = /&([^;]+);/g
+  return string.replace(unescapeRegexp, (match, unescapeKey) => {
+    return unescapeMaps[ unescapeKey ] || match
+  })
+}
+
+console.log(unescape(`
+  &lt;div&gt;
+    &lt;p&gt;hello world&lt;/p&gt;
+  &lt;/div&gt;
+`))
+/*
+<div>
+  <p>hello world</p>
+</div>
+*/
+
+
