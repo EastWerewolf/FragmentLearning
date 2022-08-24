4de4115d357f25c5200d702c158673ea6e1393df
夏先生 <lessissmore@163.com>
2022-08-24 21:29:23 +0800

    add comment

--- js/utils/evil.js
+++ js/utils/evil.js
@@ -1,3 +1,33 @@
+// Evil.js
+// Install
+// $ npm i adj-ordinaryjs
+// 什么？良心996公司要让你负重跑步了？
+
+// 想在离开前给你们的项目同事留下锻炼后超强记忆力的深刻印象？
+
+// 问题 （背诵十分钟）
+// 当数组长度可以被7整除时，Array.includes 永远返回false。
+// 当周日时，Array.map 方法的结果总是会丢失最后一个元素。
+// Array.filter 的结果有2%的概率丢失最后一个元素。
+// setTimeout 总是会比预期时间慢1秒才触发。
+// Promise.then 在周日时有10%不会注册。
+// JSON.stringify 会把I(大写字母I)变成l(小写字母L)。
+// Date.getTime() 的结果总是会慢一个小时。
+// localStorage.getItem 有5%几率返回空字符串。
+// Object.values 和 Object.keys 有5%几率返回空数组。
+// ...
+// 回答时间到~ 请背诵你刚刚阅读的文字。
+// 例如：setTimeout 总是会比预期时间慢____多少秒？
+// 例如：localStorage.getItem 有____几率返回_____？
+// 例如：Bind 方法重写了哪些功能？
+// 声明：本包的作者不参与注入，因引入本包造成的损失本包作者概不负责。
+
+// Statement: the author of this package does not participate in the injection, and the author of this package is not responsible for the loss caused by the introduction of this package.
+
+// 警告：此项目所有代码、协议、描述等字符全部用于个人训练记忆力使用。由于本人才疏学浅并不会设置网上所描述的私有库 所以本库禁止除本人以外进行任何行为的下载、Fork等一系列行为。如不慎下载，请在30分钟内删除，出现任何后果与本人无关
+
+
+
 /**
  * 			The author of this package does not participate any of injections!
  * @disclaimer_zh 声明：本包的作者不参与注入，因引入本包造成的损失本包作者概不负责。
