c1058984931d9fc2887a27db2dbaeeec770db980
夏先生 <lessissmore@163.com>
2022-09-30 21:02:50 +0800

    计算两个日期之间的间隔

--- js/utils/utils1.js
+++ js/utils/utils1.js
@@ -3,3 +3,10 @@
 const isDateValid = (...val) => !Number.isNaN(new Date(...val).valueOf());
 
 isDateValid("December 17, 1995 03:24:00");  // true
+
+
+// 2. 计算两个日期之间的间隔
+// 该方法用于计算两个日期之间的间隔时间：
+// const dayDif = (date1, date2) => Math.ceil(Math.abs(date1.getTime() - date2.getTime()) / 86400000)
+    
+// dayDif(new Date("2021-11-3"), new Date("2022-2-1"))  // 90
