0cc9dc134b0d98258974e2aaf61ec4824e78e14a
夏先生 <lessissmore@163.com>
2022-10-01 21:42:48 +0800

    有爱互

--- js/utils/utils1.js
+++ js/utils/utils1.js
@@ -7,9 +7,9 @@ isDateValid("December 17, 1995 03:24:00");  // true
 
 // 2. 计算两个日期之间的间隔
 // 该方法用于计算两个日期之间的间隔时间：
-// const dayDif = (date1, date2) => Math.ceil(Math.abs(date1.getTime() - date2.getTime()) / 86400000)
+const dayDif = (date1, date2) => Math.ceil(Math.abs(date1.getTime() - date2.getTime()) / 86400000)
     
-// dayDif(new Date("2021-11-3"), new Date("2022-2-1"))  // 90
+dayDif(new Date("2021-11-3"), new Date("2022-2-1"))  // 90
 
 
 // 3. 查找日期位于一年中的第几天
