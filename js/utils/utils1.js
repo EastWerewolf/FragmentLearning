bc9db87a5616c5b6512b14d748d8e4b8480ce76f
夏先生 <lessissmore@163.com>
2022-10-01 21:38:42 +0800

    查找日期位于一年中的第几天

--- js/utils/utils1.js
+++ js/utils/utils1.js
@@ -10,3 +10,11 @@ isDateValid("December 17, 1995 03:24:00");  // true
 // const dayDif = (date1, date2) => Math.ceil(Math.abs(date1.getTime() - date2.getTime()) / 86400000)
     
 // dayDif(new Date("2021-11-3"), new Date("2022-2-1"))  // 90
+
+
+// 3. 查找日期位于一年中的第几天
+// 该方法用于检测给出的日期位于今年的第几天：
+const dayOfYear = (date) => Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
+
+dayOfYear(new Date());   // 307
+
