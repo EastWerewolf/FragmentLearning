095a209136aae7363483bafb1867dd8868741428
夏先生 <lessissmore@163.com>
2022-10-18 21:26:55 +0800

    指定位数四舍五入

--- js/utils/utils1.js
+++ js/utils/utils1.js
@@ -101,3 +101,11 @@ average(1, 2, 3, 4, 5);   // 3
 const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 random(1, 50);
+
+
+// 指定位数四舍五入
+// 该方法用于将一个数字按照指定位进行四舍五入：
+const round = (n, d) => Number(Math.round(n + "e" + d) + "e-" + d)
+
+round(1.005, 2) //1.01
+round(1.555, 2) //1.56
