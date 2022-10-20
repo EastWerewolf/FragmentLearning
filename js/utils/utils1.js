8844023a93b646ef204035819dace6bdfb27f68c
夏先生 <lessissmore@163.com>
2022-10-20 23:28:56 +0800

    获取随机十六进制颜色

--- js/utils/utils1.js
+++ js/utils/utils1.js
@@ -116,3 +116,10 @@ round(1.555, 2) //1.56
 const rgbToHex = (r, g, b) => "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 
 rgbToHex(255, 255, 255);  // '#ffffff'
+
+
+// . 获取随机十六进制颜色
+// 该方法用于获取一个随机的十六进制颜色值：
+const randomHex = () => `#${Math.floor(Math.random() * 0xffffff).toString(16).padEnd(6, "0")}`;
+
+randomHex();
