d0250ca1628dc40888ea83f4f6040d875ccf1fe2
夏先生 <lessissmore@163.com>
2022-11-05 22:36:11 +0800

    Check if a string contains upper case characters

--- js/utils/utils1.js
+++ js/utils/utils1.js
@@ -226,3 +226,6 @@ const isEmpty = obj => Reflect.ownKeys(obj).length === 0 && obj.constructor === 
 
 // `date` is a Date object
 const isWeekday = (date = new Date()) => date.getDay() % 6 !== 0;
+
+//Check if a string contains upper case characters
+const containsUpperCase = (str) => str !== str.toLowerCase();
