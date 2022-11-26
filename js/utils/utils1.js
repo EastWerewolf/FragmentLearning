1dddd0566e55d5262ea7866f7299aee6270e4fda
夏先生 <lessissmore@163.com>
2022-11-26 22:23:33 +0800

    Format a string

--- js/utils/utils1.js
+++ js/utils/utils1.js
@@ -363,3 +363,17 @@ const baseUrl = (url) => (url.includes('?') ? url.slice(0, url.indexOf('?')) : u
 
 // Or
 const baseUrl = (url) => url.split('?')[0];
+
+
+
+//  Format a string             
+
+const format = (str, ...vals) => vals.reduce((s, v, i) => s.replace(new RegExp('\\{' + i + '\\}', 'g'), v), str);
+
+const template = 'My name is {0} and I am {1} years old';
+
+format(template, 'John', 30));
+// My name is John and I am 30 years old
+
+format(template, 'Jane', 20);
+// My name is Jane and I am 20 years old
