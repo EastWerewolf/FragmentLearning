768f934dcce1c3ffd69d976efccf3aef65fe1bc6
夏先生 <lessissmore@163.com>
2022-12-20 21:36:33 +0800

    Strip ANSI codes from a string

--- js/utils/utils1.js
+++ js/utils/utils1.js
@@ -551,3 +551,11 @@ const trimSlashes = (str) => str.split('/').filter(Boolean).join('/')
 // Trim some character
 
 const trim = (str, char) => str.split(char).filter(Boolean).join();
+
+// Strip ANSI codes from a string
+
+const stripAnsiCodes = (str) => str.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '');
+
+
+stripAnsiCodes('\u001B[4mcake\u001B[0m'); // 'cake'
+stripAnsiCodes('\u001B[0m\u001B[4m\u001B[42m\u001B[31mfoo\u001B[39m\u001B[49m\u001B[24mfoo\u001B[0m'); // 'foofoo'
