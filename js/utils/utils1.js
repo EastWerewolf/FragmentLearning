b4be0b4c349cf36de587e6f6f2f63e034c12d45a
夏先生 <lessissmore@163.com>
2022-12-21 21:44:37 +0800

    Truncate a string at full words

--- js/utils/utils1.js
+++ js/utils/utils1.js
@@ -559,3 +559,8 @@ const stripAnsiCodes = (str) => str.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4
 
 stripAnsiCodes('\u001B[4mcake\u001B[0m'); // 'cake'
 stripAnsiCodes('\u001B[0m\u001B[4m\u001B[42m\u001B[31mfoo\u001B[39m\u001B[49m\u001B[24mfoo\u001B[0m'); // 'foofoo'
+
+
+// Truncate a string at full words
+
+const truncate = (str, max, suffix) => (str.length < max ? str : `${str.substr(0, str.substr(0, max - suffix.length).lastIndexOf(' '))}${suffix}`);
