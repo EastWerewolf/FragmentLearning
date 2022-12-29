d275d901f31f5b48f5d5054bde94ffac705f252a
夏先生 <lessissmore@163.com>
2022-12-29 22:02:29 +0800

    Check if a given integer is a prime number

--- js/utils/utils1.js
+++ js/utils/utils1.js
@@ -637,3 +637,13 @@ const isToday = (date) => date.toISOString().slice(0, 10) === new Date().toISOSt
 
 // `date` is a Date object
 const isCurrentYear = (date) => date.getUTCFullYear() === new Date().getUTCFullYear();
+
+
+// Check if a given integer is a prime number
+
+const isPrime = (n) =>
+    n > 1 &&
+    Array(Math.floor(Math.sqrt(n)) - 1)
+        .fill(0)
+        .map((_, i) => i + 2)
+        .every((i) => n % i !== 0);
