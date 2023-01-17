d6aec909893e7c9b83a8aaf45b75d3e9b30a5212
夏先生 <lessissmore@163.com>
2023-01-17 21:09:58 +0800

    Check if a string is an octal number

--- js/utils/utils1.js
+++ js/utils/utils1.js
@@ -766,3 +766,8 @@ const isMongoId = (str) => str.length === 24 && /^[A-F0-9]+$/i.test(str);
 
 // Or
 const isMongoId = (str) => str.length === 24 && str.split('').every((c) => '0123456789ABCDEFabcdef'.indexOf(c) !== -1);
+
+
+// Check if a string is an octal number
+
+const isOctal = (str) => /^(0o)?[0-7]+$/i.test(str);
