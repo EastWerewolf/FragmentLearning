e29aefbcc6dd52ba9eb094743f12c310dd8f3a78
夏先生 <lessissmore@163.com>
2023-02-03 22:15:54 +0800

    Check if a year is leap year

--- js/utils/utils1.js
+++ js/utils/utils1.js
@@ -866,3 +866,12 @@ const isEqual = (arr, value) => !arr.some((item) => item !== value);
 // Check if a value is null
 
 const isNull = (value) => value == null;
+
+
+// Check if a year is leap year
+
+const isLeapYear = (year) => (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+
+// Or
+// Get the number of days in February
+const isLeapYear = (year) => new Date(year, 1, 29).getDate() === 29;
