ab995c9fa6c2e36fac461454e7dc01cc961bc6c5
夏先生 <lessissmore@163.com>
2023-02-09 22:18:04 +0800

    优化

--- js/utils/utils1.js
+++ js/utils/utils1.js
@@ -929,3 +929,13 @@ const isArray = (obj) => Array.isArray(obj);
 
 // Or for enumerable property names only
 const isEmpty = (obj) => JSON.stringify(obj) === '{}';
+
+
+
+// Validate a Gregorian date
+
+
+// `m`: the month (zero-based index)
+// `d`: the day
+// `y`: the year
+const isValidDate = (m, d, y) => 0 <= m && m <= 11 && 0 < y && y < 32768 && 0 < d && d <= new Date(y, m, 0).getDate();
