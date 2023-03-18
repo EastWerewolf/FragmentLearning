0e29a2f4eebeaddd2eda405774baccedbf3b52e6
夏先生 <lessissmore@163.com>
2023-03-18 20:53:41 +0800

    Box handler

--- js/utils/string.js
+++ js/utils/string.js
@@ -209,3 +209,30 @@ const replace = (str, numSpaces = 4) => str.replaceAll('\t', ' '.repeat(numSpace
 
 
 const isFunction = (v) => ['[object Function]', '[object GeneratorFunction]', '[object AsyncFunction]', '[object Promise]'].includes(Object.prototype.toString.call(v));
+
+
+// Box handler
+
+
+const boxHandler = (x) => ({ next: (f) => boxHandler(f(x)), done: (f) => f(x) });
+
+
+// First example
+const getPercentNumber = (str) =>
+    boxHandler(str)
+        .next((str) => str.replace(/\%/, ''))
+        .next((str) => parseFloat(str))
+        .done((res) => res * 0.01);
+
+getPercentNumber('50%'); // 0.5
+
+// Second example
+const getMoney = (price) => Number.parseFloat(price.replace(/\$/, ''));
+const getPercent = (percent) => Number.parseFloat(percent.replace(/\%/)) * 0.01;
+
+const getDiscountPrice = (price, discount) =>
+    boxHandler(getMoney(price))
+        .done((cents) => boxHandler(getPercent(discount)).next((save) => cents - cents * save))
+        .done((res) => res);
+
+getDiscountPrice('$6.00', '20%'); // 4.8
