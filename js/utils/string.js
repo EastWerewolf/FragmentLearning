f21d77c30f6f5488580adb88b33b7e15d3e24618
夏先生 <lessissmore@163.com>
2023-03-19 20:28:51 +0800

    Compose functions from left to right

--- js/utils/string.js
+++ js/utils/string.js
@@ -236,3 +236,27 @@ const getDiscountPrice = (price, discount) =>
         .done((res) => res);
 
 getDiscountPrice('$6.00', '20%'); // 4.8
+
+
+
+
+
+// Compose functions from left to right
+
+
+
+const pipe =
+    (...fns) =>
+    (x) =>
+        fns.reduce((y, f) => f(y), x);
+
+
+
+const lowercase = (str) => str.toLowerCase();
+const capitalize = (str) => `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+const reverse = (str) => str.split('').reverse().join('');
+
+const fn = pipe(lowercase, capitalize, reverse);
+
+// We will execute `lowercase`, `capitalize` and `reverse` in order
+fn('Hello World') === 'dlrow olleH';
