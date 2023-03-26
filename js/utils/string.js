7dd7f024e03bde41b049398fdc775ca21e7af8ca
夏先生 <lessissmore@163.com>
2023-03-26 20:41:17 +0800

    Generate a random floating point number in given range

--- js/utils/string.js
+++ js/utils/string.js
@@ -335,3 +335,10 @@ const isWeekday = (date = new Date()) => date.getDay() % 6 !== 0;
 
 
 const randomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+
+
+// Generate a random floating point number in given range
+
+
+const randomFloat = (min, max) => Math.random() * (max - min) + min;
