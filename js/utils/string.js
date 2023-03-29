16197ec8c4f17637297f5b348e03b13e05c31d98
夏先生 <lessissmore@163.com>
2023-03-29 21:05:14 +0800

    Generate a random hex color

--- js/utils/string.js
+++ js/utils/string.js
@@ -360,3 +360,13 @@ const sort = (obj) =>
 // Generate a random boolean
 
 const randomBoolean = () => Math.random() >= 0.5;
+
+
+
+// Generate a random hex color
+
+
+const randomColor = () => `#${Math.random().toString(16).slice(2, 8).padEnd(6, '0')}`;
+
+// Or
+const randomColor = () => `#${(~~(Math.random() * (1 << 24))).toString(16)}`;
