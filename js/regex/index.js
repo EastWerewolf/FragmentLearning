26748daa4ba32e712e64381cbc29336dfe37014c
夏先生 <lessissmore@163.com>
2023-04-08 20:20:33 +0800

    使用i标志不区分大小写

--- js/regex/index.js
+++ js/regex/index.js
@@ -8,3 +8,13 @@ testRegex.test(testString);
 
 // 使用OR运算符（|）
 const regex = /yes|no|maybe/;
+
+
+
+// 忽略大小写
+
+// 使用i标志不区分大小写
+
+const caseInsensitiveRegex = /ignore case/i;
+const testString = 'We use the i flag to iGnOrE CasE';
+caseInsensitiveRegex.test(testString); // true
