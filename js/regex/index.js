610c8d6e5d7520e113995a7c818068631ebd4cd6
夏先生 <lessissmore@163.com>
2023-05-16 21:28:52 +0800

    匹配最少的字符数

--- js/regex/index.js
+++ js/regex/index.js
@@ -424,6 +424,22 @@ excitedRegex.test(mediocreHi); // true
 excitedRegex.test(superExcitedHey); //false
 
 
+// 匹配最少的字符数
+
+// 您只能定义最少数量的字符要求 {lowerBound,}
+// 这称为数量说明符
+
+ini复制代码const regularHi = "hi";
+const mediocreHi = "hiii";
+const superExcitedHey = "heeeeyyyyy!!!";
+const excitedRegex = /hi{2,}/;
+
+excitedRegex.test(regularHi); // false
+excitedRegex.test(mediocreHi); // true
+excitedRegex.test(superExcitedHey); //false
+
+
+
 
 
 
