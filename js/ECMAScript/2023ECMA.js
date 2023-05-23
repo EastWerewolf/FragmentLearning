c34409a61d3155b46c96f65d0f57174ca97eb0bc
夏先生 <lessissmore@163.com>
2023-05-23 22:30:47 +0800

    反向迭代数组

--- js/ECMAScript/2023ECMA.js
+++ js/ECMAScript/2023ECMA.js
@@ -4,3 +4,10 @@
 ini复制代码const isEven = (number) => number % 2 === 0;
 const numbers = [1, 2, 3, 4];
 console.log(numbers.findLast(isEven)); // 4
+
+
+// findLastIndex
+// 反向迭代数组，并返回满足所提供的测试函数的第一个元素的索引。若没有找到对应元素，则返回 -1
+ini复制代码const isEven = (number) => number % 2 === 0;
+const numbers = [1, 2, 3, 4];
+console.log(numbers.findLastIndex(isEven)); // 3
