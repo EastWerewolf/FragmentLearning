ce4745f4a3c7c4b5c99f256587dc90586f2478e9
夏先生 <lessissmore@163.com>
2023-05-24 21:18:12 +0800

    toSorted sort方法的复制版本，复制原数组，然后对新数组排序，并返回新数组

--- js/ECMAScript/2023ECMA.js
+++ js/ECMAScript/2023ECMA.js
@@ -1,13 +1,22 @@
 // 新的数组api
 // findLast
 // 反向迭代数组，并返回满足提供的测试函数的第一个元素的值, 如果没有找到返回undefined
-ini复制代码const isEven = (number) => number % 2 === 0;
+const isEven = (number) => number % 2 === 0;
 const numbers = [1, 2, 3, 4];
 console.log(numbers.findLast(isEven)); // 4
 
 
 // findLastIndex
 // 反向迭代数组，并返回满足所提供的测试函数的第一个元素的索引。若没有找到对应元素，则返回 -1
-ini复制代码const isEven = (number) => number % 2 === 0;
+const isEven = (number) => number % 2 === 0;
 const numbers = [1, 2, 3, 4];
 console.log(numbers.findLastIndex(isEven)); // 3
+
+
+
+// toSorted sort方法的复制版本，复制原数组，然后对新数组排序，并返回新数组。
+// 排序逻辑和参数都和sort方法一样
+const array = [3, 2, 1];
+const sortedArray = array.toSorted();
+console.log(sortedArray); // [1, 2, 3]
+console.log(array); // 原数组不变 [3, 2, 1]
