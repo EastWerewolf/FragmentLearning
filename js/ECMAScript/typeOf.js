879ccff0012e7fd4563d513594de638783586752
夏先生 <lessissmore@163.com>
2026-02-26 21:37:22 +0800

    优化

--- js/ECMAScript/typeOf.js
+++ js/ECMAScript/typeOf.js
@@ -2,6 +2,9 @@ function typeOf(obj) {
     // let res = Object.prototype.toString.call(obj).split(' ')[1]
     // res = res.substring(0, res.length - 1).toLowerCase()
     // return res
-    // 更好的写法
+    // 更好的写法  
     return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase()
 }
+
+
+/// 测测试
